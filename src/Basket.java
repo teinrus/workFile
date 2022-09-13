1ef2fdc1307f6f0f6b2ab870a84c1e91aebaa877
teinrus <Tnrkip@yandex.ru>
2022-09-13 16:57:29 +0300

    delete old method

--- src/Basket.java
+++ src/Basket.java
@@ -5,7 +5,7 @@ public class Basket implements Serializable {
 
     private static List<String> products;
     private static List<Integer> prices;
-    private static Map<Integer,Integer> totalPrice=new HashMap<>();
+    private static Map<Integer, Integer> totalPrice = new HashMap<>();
 
     public Basket(List<String> products, List<Integer> prices) {
         this.products = products;
@@ -16,14 +16,15 @@ public class Basket implements Serializable {
 
     }
 
-    public void addToCart(int productNum, int amount){
-        if (totalPrice.containsKey(productNum)){
-            totalPrice.put(productNum,totalPrice.get(productNum)+amount);
-        }else {
-            totalPrice.put(productNum,amount);
+    public void addToCart(int productNum, int amount) {
+        if (totalPrice.containsKey(productNum)) {
+            totalPrice.put(productNum, totalPrice.get(productNum) + amount);
+        } else {
+            totalPrice.put(productNum, amount);
         }
 
     }
+
     public void printCart() {
         for (int i = 0; i < getProducts().size(); i++) {
             // Проверка на введенные продукты и печатаем только которые купили
@@ -31,7 +32,7 @@ public class Basket implements Serializable {
                 System.out.println(getProducts().get(i) + ": "
                         + (getTotalPrice().get(i))
                         + " шт " + getPrices().get(i) + " руб/шт "
-                        + getTotalPrice().get(i)*getPrices().get(i) + " руб в сумме");
+                        + getTotalPrice().get(i) * getPrices().get(i) + " руб в сумме");
             }
         }
     }
@@ -39,11 +40,11 @@ public class Basket implements Serializable {
 
     public void saveBin(File file) throws IOException {
 
-        Basket saveBin =new Basket();
+        Basket saveBin = new Basket();
         try (FileOutputStream os = new FileOutputStream(file);
-         ObjectOutputStream oos = new ObjectOutputStream(os)) {
+             ObjectOutputStream oos = new ObjectOutputStream(os)) {
 
-             oos.writeObject(saveBin);
+            oos.writeObject(saveBin);
 
         } catch (Exception ex) {
             System.out.println("ошибка");
@@ -51,36 +52,11 @@ public class Basket implements Serializable {
         System.out.println("Данные сохранены");
         System.out.println(saveBin);
     }
-    static Basket loadFromTxtFile(File textFile){
-        try {
-            File file = new File(textFile.toURI());
-            //создаем объект FileReader для объекта File
-            FileReader fr = new FileReader(file);
-            //создаем BufferedReader с существующего FileReader для построчного считывания
-            BufferedReader reader = new BufferedReader(fr);
-            // считаем сначала первую строку
-            String line = reader.readLine();
-            int i=0;
-            while (line != null) {
-                System.out.println(line);
-                String[] pars = line.split(" ");
-                totalPrice.put(i, Integer.valueOf(pars[1]));
-                i++;
-                // считываем остальные строки в цикле
-                line = reader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        return null;
-    }
     static void loadFromBinFile(File file) {
         Basket basket = new Basket();
-        try (FileInputStream  fis = new FileInputStream(file);
-         ObjectInputStream ois = new ObjectInputStream(fis)) {
+        try (FileInputStream fis = new FileInputStream(file);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
             basket = (Basket) ois.readObject();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
@@ -100,17 +76,12 @@ public class Basket implements Serializable {
         return prices;
     }
 
-    public void setPrices(List<Integer> prices) {
-        this.prices = prices;
-    }
+
 
     public Map<Integer, Integer> getTotalPrice() {
         return totalPrice;
     }
 
-    public void setTotalPrice(Map<Integer, Integer> totalPrice) {
-        this.totalPrice = totalPrice;
-    }
 
     @Override
     public String toString() {
