82709c62d9ba7c20f36390e288c443d758570297
teinrus <Tnrkip@yandex.ru>
2022-09-13 14:56:02 +0300

    added class Basket.java and method save and load;

---
+++ src/Basket.java
@@ -0,0 +1,115 @@
+import java.io.*;
+import java.util.*;
+
+public class Basket {
+
+    private static List<String> products;
+    private static List<Integer> prices;
+    private static Map<Integer,Integer> totalPrice=new HashMap<>();
+
+    public Basket(List<String> products, List<Integer> prices) {
+        this.products = products;
+        this.prices = prices;
+    }
+
+    public void addToCart(int productNum, int amount){
+        if (totalPrice.containsKey(productNum)){
+            totalPrice.put(productNum,totalPrice.get(productNum)+amount);
+        }else {
+            totalPrice.put(productNum,amount);
+        }
+
+    }
+    public void printCart() {
+        for (int i = 0; i < getProducts().size(); i++) {
+            // Проверка на введенные продукты и печатаем только которые купили
+            if (getTotalPrice().get(i) != null) {
+                System.out.println(getProducts().get(i) + ": "
+                        + (getTotalPrice().get(i))
+                        + " шт " + getPrices().get(i) + " руб/шт "
+                        + getTotalPrice().get(i)*getPrices().get(i) + " руб в сумме");
+            }
+        }
+    }
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(textFile);){
+            for (int i = 0; i < getProducts().size(); i++) {
+                if (getTotalPrice().get(i) != null){
+                    String temp = getProducts().get(i)+" "+getTotalPrice().get(i)+
+                            " "+getPrices().get(i);
+
+                    out.print(temp);
+                    out.print('\n');
+                }else {
+                    String temp = getProducts().get(i)+" "+0+" "+getPrices().get(i);
+                    out.print(temp);
+                    out.print('\n');
+
+                }
+
+            }
+        }
+        System.out.println("Данные сохранены");
+
+    }
+    static Basket loadFromTxtFile(File textFile){
+        try {
+            File file = new File(textFile.toURI());
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            int i=0;
+            while (line != null) {
+                System.out.println(line);
+                String[] pars = line.split(" ");
+                totalPrice.put(i, Integer.valueOf(pars[1]));
+                i++;
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public List<String> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<String> products) {
+        this.products = products;
+    }
+
+    public List<Integer> getPrices() {
+        return prices;
+    }
+
+    public void setPrices(List<Integer> prices) {
+        this.prices = prices;
+    }
+
+    public Map<Integer, Integer> getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Map<Integer, Integer> totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Baske{" +
+                "products=" + products +
+                ", prices=" + prices +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+// создание статического массива
+        List<String> products = List.of("Хлеб", "Яблоки", "Молоко");
+        List<Integer> prices = List.of(100, 200, 300);
+        int[] currentPrice = new int[3];
+        int[] countProduct = new int[3];
+
+
+        Basket baske = new Basket(products, prices);
+        System.out.println(baske);
+
+
+// переменные
+        int sumProducts = 0;
+// создаем сканер
+
+        Scanner scan = new Scanner(System.in);
+// Первое отображение списка покупок
+        System.out.println("Список возможных товаров для покупки");
+        for (int i = 0; i < baske.getProducts().size(); i++) {
+            System.out.println((i + 1) + "." + baske.getProducts().get(i) + " " + baske.getPrices().get(i) + "р/шт");
+        }
+// бесконечный цикл
+        while (true) {
+
+            System.out.println("Выберите товар и количество или введите `end`для завершения или " +
+                    "'load' для загрузки файлов");
+            String input = scan.nextLine();
+
+            if ("end".equals(input)) {
+                // Общая сумма
+
+
+                for (int i = 0; i < baske.getTotalPrice().size(); i++) {
+                    if (baske.getTotalPrice().get(i) != null){
+                        sumProducts += baske.getTotalPrice().get(i)*baske.getPrices().get(i);
+                    }
+                }
+                System.out.println(baske);
+
+                // Вывод на печать
+                    baske.printCart();
+                    System.out.println("Итого: " + sumProducts + " руб");
+                    break;
+
+
+            }
+            if ("load".equals(input)) {
+                baske.loadFromTxtFile(new File("test.txt"));
+                System.out.println(baske);
+                System.out.println("Успешно загружены ");
+                continue;
+            }
+                String[] pars = input.split(" ");
+                int productNumber = Integer.parseInt(pars[0]) - 1;
+                int productCount = Integer.parseInt(pars[1]);
+
+//Стоимость продукта
+                baske.addToCart(productNumber, productCount);
+                baske.saveTxt(new File("test.txt"));
+            }
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
