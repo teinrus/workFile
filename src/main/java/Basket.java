1a3fbc265afdd7cc8dae675c10d661eee5d12647
teinrus <Tnrkip@yandex.ru>
2022-09-14 16:38:16 +0300

    added config file

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -2,7 +2,6 @@
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
 import java.util.HashMap;
@@ -41,7 +40,7 @@ public class Basket {
         }
     }
 
-    public void saveTxt(File textFile) {
+    public void saveJSon(File textFile) {
         try (Writer writer= new FileWriter(textFile)){
             Gson gson = new Gson();
             Basket temp = new Basket(products, prices);
@@ -52,18 +51,69 @@ public class Basket {
             throw new RuntimeException(e);
         }
     }
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(textFile);) {
+            for (int i = 0; i < getProducts().size(); i++) {
+                if (getTotalPrice().get(i) != null) {
+                    String temp = getProducts().get(i) + " " + getTotalPrice().get(i) +
+                            " " + getPrices().get(i);
 
-    public static void  loadFromTxtFile(File textFile) throws RuntimeException {
+                    out.print(temp);
+                    out.print('\n');
+                } else {
+                    String temp = getProducts().get(i) + " " + 0 + " " + getPrices().get(i);
+                    out.print(temp);
+                    out.print('\n');
+
+                }
+
+            }
+        }
+        System.out.println("Данные сохранены");
+
+    }
+
+
+
+    public void  loadFromJSonFile(File textFile) throws RuntimeException {
     try (Reader reader = new FileReader(textFile)) {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
         Basket temp = gson.fromJson(reader,Basket.class);
+        this.totalPrice=temp.totalPrice;
         System.out.println(temp);
         System.out.println("Данные загружены");
     } catch (Exception e) {
+
         System.out.println("Файл не найден");;
     }
 
+    }
+    public Basket loadFromTxtFile(File textFile) {
+
+        File file = new File(textFile.toURI());
+        //создаем объект FileReader для объекта File
+        try (FileReader fr = new FileReader(file);) {
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            int i = 0;
+            while (line != null) {
+                System.out.println(line);
+                String[] pars = line.split(" ");
+                this.totalPrice.put(i, Integer.valueOf(pars[1]));
+                i++;
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public List<String> getProducts() {
@@ -98,5 +148,6 @@ public class Basket {
                 ", totalPrice=" + totalPrice +
                 '}';
     }
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,84 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
+
+
 public class Main {
 
 
     public static void main(String[] args) throws IOException {
+        Boolean loadCon=true;
+        String fileNameLoad = null;
+        String fileFormatLoad=null;
+
+        Boolean saveCon=true;
+        String fileNameSave = null;
+        String fileFormatSave=null;
+
+        Boolean logCon=true;
+        String fileNameLog = null;
+
+        try {
+            File fXmlFile = new File("shop.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("load");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    loadCon= Boolean.valueOf(eElement.getElementsByTagName("enabled")
+                            .item(0).getTextContent());
+                    fileNameLoad=eElement.getElementsByTagName("fileName")
+                            .item(0).getTextContent();
+                    fileFormatLoad= eElement.getElementsByTagName("format")
+                            .item(0).getTextContent();
+                }
+            }
+            NodeList nList2 = doc.getElementsByTagName("save");
+            for (int temp = 0; temp < nList2.getLength(); temp++) {
+                Node nNode = nList2.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    saveCon= Boolean.valueOf(eElement.getElementsByTagName("enabled")
+                            .item(0).getTextContent());
+                    fileNameSave=eElement.getElementsByTagName("fileName")
+                            .item(0).getTextContent();
+                    fileFormatSave=eElement.getElementsByTagName("format")
+                            .item(0).getTextContent();
+                }
+            }
+            NodeList nList3 = doc.getElementsByTagName("log");
+
+            for (int temp = 0; temp < nList2.getLength(); temp++) {
+                Node nNode = nList3.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Load enabled : " +
+                            eElement.getElementsByTagName("enabled").item(0).getTextContent());
+                    System.out.println("fileName  : " +
+                            eElement.getElementsByTagName("fileName").item(0).getTextContent());
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 // создание статического массива
         List<String> products = List.of("Хлеб", "Яблоки", "Молоко");
         List<Integer> prices = List.of(100, 200, 300);
@@ -16,8 +86,14 @@ public class Main {
 
         Basket baske = new Basket(products, prices);
         System.out.println(baske);
+        if (fileFormatLoad.equals("json") && loadCon){
+            baske.loadFromJSonFile(new File(fileNameLoad));
+        }else if(loadCon){
+            baske.loadFromTxtFile(new File(fileNameLoad));
+        }
 
-        Basket.loadFromTxtFile(new File("basket.json"));
+
+       //
 // переменные
         int sumProducts = 0;
 
@@ -50,22 +126,47 @@ public class Main {
                 // Вывод на печать
                 baske.printCart();
                 System.out.println("Итого: " + sumProducts + " руб");
-                clientLog.exportAsCSV(new File("log.csv"));
+
+                if (fileFormatSave.equals("json")){
+                    baske.saveJSon(new File(fileNameSave));
+                }else{
+                    baske.saveTxt(new File(fileNameSave));
+                }
+
+                if (logCon){
+                    clientLog.exportAsCSV(new File(fileNameLog));
+                }
                 break;
 
 
             }
             if ("load".equals(input)) {
-                baske.loadFromTxtFile(new File("basket.json"));
+                if (fileFormatLoad.equals("json")){
+                    baske.loadFromJSonFile(new File(fileNameLoad));
+                }else{
+                    baske.loadFromTxtFile(new File(fileNameLoad));
+                }
+
                 continue;
             }
             String[] pars = input.split(" ");
             int productNumber = Integer.parseInt(pars[0]) - 1;
             int productCount = Integer.parseInt(pars[1]);
-            clientLog.log(productNumber,productCount);
+
+
+                clientLog.log(productNumber,productCount);
+
+
 //Стоимость продукта
             baske.addToCart(productNumber, productCount);
-            baske.saveTxt(new File("basket.json"));
+
+            if (fileFormatSave.equals("json") && saveCon){
+                baske.saveJSon(new File(fileNameSave));
+            }else if(saveCon){
+                baske.saveTxt(new File(fileNameSave));
+            }
+
+
         }
 
 
